618b47d1f9f81ddd2cf13ba2e1ad4822e02263d6
Alicia Boltryk <boltryk.alicia@gmail.com>
2022-09-25 15:19:11 +0200

    modern-hmi

--- tutorials/pizzeria/hmi/modern/src/index.js
+++ tutorials/pizzeria/hmi/modern/src/index.js
@@ -1,2 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+import 'animate.css'; // pour que ça ait marché, j'ai eu besoin de npm install animate.css --save
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', startOrStopSound);
+
+function startOrStopSound() {
+  const myAudioPlayer = document.querySelector('#audioPlayer');
+
+  if (myAudioPlayer.paused) myAudioPlayer.play();
+  else myAudioPlayer.pause();
+}
