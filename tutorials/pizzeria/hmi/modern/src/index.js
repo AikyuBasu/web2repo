b2355dcf8efecb0989406c29daa4ba0980c82828
Alicia Boltryk <boltryk.alicia@gmail.com>
2022-09-25 17:18:20 +0200

    modern-esthetic-hmi

--- tutorials/pizzeria/hmi/modern/src/index.js
+++ tutorials/pizzeria/hmi/modern/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 
+
 import 'animate.css'; // pour que ça ait marché, j'ai eu besoin de npm install animate.css --save
 
 const body = document.querySelector('body');
