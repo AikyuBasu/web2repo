b2229ab51b1b4bced1a7e6ed630c96e7c8df6163
Alicia Boltryk <boltryk.alicia@gmail.com>
2022-10-10 12:37:45 +0200

    express-static-file-server

---
+++ tutorials/express-static-file-server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 7777;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.use(express.static('public'));
+
